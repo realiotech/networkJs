f9f976838dd2464069a51de5c733d4fcdc98b69f
jiujiteiro <romeiroe@gmail.com>
2022-06-17 13:13:58 -0400

    lint

--- packages/address-generator/src/generator.ts
+++ packages/address-generator/src/generator.ts
@@ -1,6 +1,6 @@
 import { Wallet } from '@ethersproject/wallet'
 
-export const generateMnemonic = () : string => {
-  const wallet = Wallet.createRandom();
-  return wallet.mnemonic.phrase;
+export const generateMnemonic = (): string => {
+  const wallet = Wallet.createRandom()
+  return wallet.mnemonic.phrase
 }
