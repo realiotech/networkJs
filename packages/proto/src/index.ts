7e0b72bb3485ea418fc5204f4f56a7d4de81edf0
jiujiteiro <romeiroe@gmail.com>
2022-07-06 14:32:47 -0400

    export

--- packages/proto/src/index.ts
+++ packages/proto/src/index.ts
@@ -1,4 +1,7 @@
 export * from './messages/msgSend'
+export * from './messages/ibcMsgTransfer'
+export * from './messages/validator'
+export * from './messages/staking'
 export * from './messages/utils'
 export * from './messages/txRaw'
 export * from './transaction/transaction'
